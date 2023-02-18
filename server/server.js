6d1e4fa33354a61b0587fcba1463dbd8f924e0da
Lucca Mancusi <luccaromano@gmail.com>
2023-02-18 18:38:13 -0300

    Api - CRUD com firestore

--- server/server.js
+++ server/server.js
@@ -1,24 +1,103 @@
 const express = require("express");
 const path = require("path");
 
+const admin = require("firebase-admin");
+
+const credentials = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
+
+const db = admin.firestore();
+
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/create", async (req, res) => {
+  try {
+    const productJson = {
+      name: req.body.name,
+      price: req.body.price,
+      urlImage: req.body.urlImage,
+    };
+    const response = await db.collection("products").add(productJson);
+    console.log(response);
+    res.send(response);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("/get/all", async (req, res) => {
+  try {
+    const productsRef = db.collection("products");
+    const response = await productsRef.get();
+    let responseArr = [];
 
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (error) {
-      if (error) {
-        res.status(500).send(error);
-      }
-    }
-  );
+    response.forEach((doc) => {
+      responseArr.push(doc.data());
+    });
+    res.send(responseArr);
+  } catch (err) {
+    res.send(err);
+  }
 });
+
+app.get("/get/:id", async (req, res) => {
+  try {
+    const productRef = db.collection("products").doc(req.params.id);
+    const response = await productRef.get();
+
+    res.send(response.data());
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.put("/update", async (req, res) => {
+  try {
+    const id = req.body.id;
+    const userJson = {
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    };
+
+    const userRef = await db.collection("products").doc(id).update(userJson);
+    res.send(userRef);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const response = await db
+      .collection("products")
+      .doc(req.params.id)
+      .delete();
+    res.send(response);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// app.use(express.static(path.join(__dirname, "../client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function (error) {
+//       if (error) {
+//         res.status(500).send(error);
+//       }
+//     }
+//   );
+// });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("running");
