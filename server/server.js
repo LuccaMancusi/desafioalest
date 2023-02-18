1e523b8ce88aba759344d6aff5b7bd99c3f1cc31
Lucca Mancusi <luccaromano@gmail.com>
2023-02-18 15:51:54 -0300

    build test

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (error) {
+      if (error) {
+        res.status(500).send(error);
+      }
+    }
+  );
 });
 
 app.listen(5000, () => {
